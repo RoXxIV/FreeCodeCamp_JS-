07136cd38ccfa051355a174bd94baac898c11bef
evan <evan.hermier@gmail.com>
2024-11-01 23:24:51 +0100

    Sum digits of an integer

---
+++ Rosetta/SumDigitsOfAnInteger.js
@@ -0,0 +1,20 @@
+/**
+ *  - Sum digits of an integer
+ * 
+
+Write a function that takes a string as a parameter. This string represents a number that can be in any base (less than 37) and return the sum of its digits.
+
+    110 sums to 1
+    123410 sums to 10
+    fe16 sums to 29
+    f0e16 sums to 29
+
+
+ */
+function sumDigits(n) {
+  let sum = 0;
+  for (let char of n) {
+    sum += parseInt(char, 36);
+  }
+  return sum;
+}
